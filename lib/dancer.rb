22ea56869e4e11fae16ee2c3fc1deb39ba737ae6
Cesar Hernandez <chema27281@gmail.com>
2022-11-27 15:02:40 -0500

    commit tree 8adb80651630f68e4b11ed288402604325573309

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,9 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+class Dancer
+    attr_accessor :name
+    extend MetaDancing
+    def initialize(name)
+      @name = name
+    end
+end
